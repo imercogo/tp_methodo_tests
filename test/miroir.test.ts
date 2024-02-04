53f2360da32adb60a3a19a899bb5349e1cd2d2b6
imercogo <remi.cogo@hotmail.fr>
2024-02-04 15:29:10 +0100

    Create first test miroir

---
+++ test/miroir.test.ts
@@ -0,0 +1,15 @@
+describe('test works', () => {
+    test.each([
+        ['test'],
+        ['radar'],
+        ['coucou'],
+        ['hello']
+    ])('QUAND on saisit une chaine %s ' + 
+    'ALORS elle est renvoyée en miroir',
+    (chaine : any) => {
+
+        let attendu = chaine.split('').reverse().join('');
+        let resultat = new VerificateurChaine().verifier(chaine);
+        expect(resultat).toContain(attendu);
+    });
+})
