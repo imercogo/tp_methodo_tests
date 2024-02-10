8ae1213e21c0db8cd0097363084276940d4dba09
imercogo <remi.cogo@hotmail.fr>
2024-02-10 15:36:54 +0100

    Add third test recette

--- test/recette.test.ts
+++ test/recette.test.ts
@@ -60,4 +60,58 @@ describe('test recette', () => {
         expect(derniereLigne).toEqual(français.quitter(moment))
 
     })
+
+    test.each([
+        ['girafarig'],
+        ['engagelejeuquejelegagne']
+    ])('ÉTANT donné un utilisateur parlant une langue inconnue ' +
+    'ET que la période de la journée est la nuit ' +
+    'QUAND il saisit un palindrome ' +
+    "ALORS on lui dit Bonne nuit, puis on renvoie le palindrome, suivi de bien dit et de au revoir dans la langue par défaut",
+    (chaine: any) => {
+        const moment: MomentDeLaJournee = MomentDeLaJournee.NUIT;
+        const français : LangueFrançaise = new LangueFrançaise();
+
+        const resultat = new VerificateurChaineBuilder()
+                                                        .AyantPourMomentDeLaJournee(moment)
+                                                        .Build().verifier(chaine);
+
+        var premiereLigne = resultat.split(os.EOL)[0];                                                
+        var attendu = resultat.split(os.EOL)[1];
+        var reverseChaine = chaine.split('').reverse().join('');
+        var felicitations = resultat.split(os.EOL)[2];
+        var derniereLigne = resultat.split(os.EOL)[3];
+
+        expect(premiereLigne).toEqual(français.saluer(moment))
+        expect(attendu).toEqual(reverseChaine);
+        expect(felicitations).toEqual(français.feliciter())
+        expect(derniereLigne).toEqual(français.quitter(moment))
+    })  
+    
+    test.each([
+        ['girafarig'],
+        ['engagelejeuquejelegagne']
+    ])('ÉTANT donné un utilisateur parlant une langue inconnue ' +
+    'ET que la période de la journée est la nuit ' +
+    'QUAND il saisit un palindrome ' +
+    "ALORS on lui dit Bonne nuit, puis on renvoie le palindrome, suivi de bien dit et de au revoir dans la langue par défaut",
+    (chaine: any) => {
+        const moment: MomentDeLaJournee = MomentDeLaJournee.NUIT;
+        const français : LangueFrançaise = new LangueFrançaise();
+
+        const resultat = new VerificateurChaineBuilder()
+                                                        .AyantPourMomentDeLaJournee(moment)
+                                                        .Build().verifier(chaine);
+
+        var premiereLigne = resultat.split(os.EOL)[0];                                                
+        var attendu = resultat.split(os.EOL)[1];
+        var reverseChaine = chaine.split('').reverse().join('');
+        var felicitations = resultat.split(os.EOL)[2];
+        var derniereLigne = resultat.split(os.EOL)[3];
+
+        expect(premiereLigne).toEqual(français.saluer(moment))
+        expect(attendu).toEqual(reverseChaine);
+        expect(felicitations).toEqual(français.feliciter())
+        expect(derniereLigne).toEqual(français.quitter(moment))
+    })
 })
